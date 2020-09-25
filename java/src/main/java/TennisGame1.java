bfda0e879ef6385d7248ae5038b0202dabc4ec61
martin-garcia-blanco <programadopormartin@gmail.com>
2020-09-25 11:10:36 +0200

    :recycle: Extract methods to calculate the score

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,86 +1,104 @@
 
 public class TennisGame1 implements TennisGame {
 
-  private int scorePlayerOne = 0;
-  private int scorePlayerTwo = 0;
-  private String playerOne;
-  private String playerTwo;
+    private int scorePlayerOne = 0;
+    private int scorePlayerTwo = 0;
+    private String playerOne;
+    private String playerTwo;
 
-  public TennisGame1(String playerOne, String playerTwo) {
-    this.playerOne = playerOne;
-    this.playerTwo = playerTwo;
-  }
-
-  public void wonPoint(String playerName) {
-    if (playerName.equals(playerOne)) {
-      scorePlayerOne += 1;
-    } else {
-      scorePlayerTwo += 1;
+    public TennisGame1(String playerOne, String playerTwo) {
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
     }
-  }
 
-  public String getScore() {
-    StringBuilder result = new StringBuilder();
-    int tempScore = 0;
-    if (isADraw(scorePlayerOne, scorePlayerTwo)) {
-      switch (scorePlayerOne) {
-        case 0:
-          result = new StringBuilder("Love-All");
-          break;
-        case 1:
-          result = new StringBuilder("Fifteen-All");
-          break;
-        case 2:
-          result = new StringBuilder("Thirty-All");
-          break;
-        default:
-          result = new StringBuilder("Deuce");
-          break;
-
-      }
-    } else if (hasAnyPlayerScoredForty()) {
-      int minusResult = scorePlayerOne - scorePlayerTwo;
-      if (isADraw(minusResult, 1)) {
-        result = new StringBuilder("Advantage " + playerOne);
-      } else if (isADraw(minusResult, -1)) {
-        result = new StringBuilder("Advantage " + playerTwo);//fix me string duplication
-      } else if (minusResult >= 2) {
-        result = new StringBuilder("Win for " + playerOne);
-      } else {
-        result = new StringBuilder("Win for " + playerTwo);
-      }
-    } else {
-      for (int i = 1; i < 3; i++) {
-        if (isADraw(i, 1)) {
-          tempScore = scorePlayerOne;
+    public void wonPoint(String playerName) {
+        if (playerName.equals(playerOne)) {
+            scorePlayerOne += 1;
         } else {
-          result.append("-");
-          tempScore = scorePlayerTwo;
+            scorePlayerTwo += 1;
         }
-        switch (tempScore) {
-          case 0:
-            result.append("Love");
-            break;
-          case 1:
-            result.append("Fifteen");
-            break;
-          case 2:
-            result.append("Thirty");
-            break;
-          case 3:
-            result.append("Forty");
-            break;
-        }
-      }
     }
-    return result.toString();
-  }
 
-  private boolean isADraw(int scorePlayerOne, int scorePlayerTwo) {
-    return scorePlayerOne == scorePlayerTwo;
-  }
+    public String getScore() {
+        StringBuilder result;
+        if (isADraw(scorePlayerOne, scorePlayerTwo)) {
+            result = calculateDrawScore();
+        } else if (hasAnyPlayerScoredForty()) {
+            result = calculateScoreWhenSomePlayerReachedForty();
+        } else {
+            result = calculateScore();
+        }
+        return result.toString();
+    }
 
-  private boolean hasAnyPlayerScoredForty() {
-    return scorePlayerOne >= 4 || scorePlayerTwo >= 4;
-  }
+    private StringBuilder calculateScore() {
+        StringBuilder result = new StringBuilder();
+        int tempScore;
+        for (int i = 1; i < 3; i++) {
+            if (isADraw(i, 1)) {
+                tempScore = scorePlayerOne;
+            } else {
+                result.append("-");
+                tempScore = scorePlayerTwo;
+            }
+            switch (tempScore) {
+                case 0:
+                    result.append("Love");
+                    break;
+                case 1:
+                    result.append("Fifteen");
+                    break;
+                case 2:
+                    result.append("Thirty");
+                    break;
+                case 3:
+                    result.append("Forty");
+                    break;
+            }
+        }
+        return result;
+    }
+
+    private StringBuilder calculateScoreWhenSomePlayerReachedForty() {
+        StringBuilder result;
+        int minusResult = scorePlayerOne - scorePlayerTwo;
+        if (isADraw(minusResult, 1)) {
+            result = new StringBuilder("Advantage " + playerOne);
+        } else if (isADraw(minusResult, -1)) {
+            result = new StringBuilder("Advantage " + playerTwo);//fix me string duplication
+        } else if (minusResult >= 2) {
+            result = new StringBuilder("Win for " + playerOne);
+        } else {
+            result = new StringBuilder("Win for " + playerTwo);
+        }
+        return result;
+    }
+
+    private StringBuilder calculateDrawScore() {
+        StringBuilder result;
+        switch (scorePlayerOne) {
+            case 0:
+                result = new StringBuilder("Love-All");
+                break;
+            case 1:
+                result = new StringBuilder("Fifteen-All");
+                break;
+            case 2:
+                result = new StringBuilder("Thirty-All");
+                break;
+            default:
+                result = new StringBuilder("Deuce");
+                break;
+
+        }
+        return result;
+    }
+
+    private boolean isADraw(int scorePlayerOne, int scorePlayerTwo) {
+        return scorePlayerOne == scorePlayerTwo;
+    }
+
+    private boolean hasAnyPlayerScoredForty() {
+        return scorePlayerOne >= 4 || scorePlayerTwo >= 4;
+    }
 }
